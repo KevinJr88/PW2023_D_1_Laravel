3051e57925c7e6cde2a31be25d68be847a9e9fa8
tikorst <74393614+tikorst@users.noreply.github.com>
2023-12-20 15:46:30 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('REDIRECT_HTTPS'))
         {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 }
